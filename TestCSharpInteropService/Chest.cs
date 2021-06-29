07ab693a10138bd106c424fc1b6d308e3f33346d
Danilin Aleksandr <Fodster@rambler.ru>
2021-06-29 16:57:16 +0300

    Убрал все константы в соответствующий класс

--- TestCSharpInteropService/Chest.cs
+++ TestCSharpInteropService/Chest.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestCSharpInteropService {
+﻿namespace TestCSharpInteropService {
     class Chest {
         internal readonly string dllFile;
         internal readonly string className;
-        internal readonly string methodName;
+
+        internal const int IntFirst = 1;
+        internal const int IntSecond = 2;
+
+        internal const double DoubleFirst = 1.0;
+        internal const double DoubleSecond = 2;
+
+        internal const string StringFirst = "first";
+        internal const string StringSecond = "second";
+
+        internal const string InputIntReturnArray = "InputIntReturnArray";
+        internal const string InputStringReturnArray = "InputStringReturnArray";
+        internal const string InputDoubleReturnArray = "InputDoubleReturnArray";
+        internal const string InputObjectReturnArray = "InputObjectReturnArray";
+        internal const string InputVoidReturnArray = "InputVoidReturnArray";
 
         public Chest() {
             dllFile = HelpTestLibrary.HelpTestClass.GetAddres();
             className = "HelpTestLibrary.HelpTestClass";
-            methodName = "";
         }
     }
 }
--- TestCSharpInteropService/TestLibraryInvoke.cs
+++ TestCSharpInteropService/TestLibraryInvoke.cs
@@ -3,11 +3,10 @@ using NUnit.Framework;
 
 namespace TestCSharpInteropService {
     public class TestLibraryInvoke {
-        private Chest _chest;
+        private static Chest _chest = new Chest();
 
         [SetUp]
         public void Setup() {
-            _chest = new Chest();
         }
 
         [Test]
@@ -22,24 +21,24 @@ namespace TestCSharpInteropService {
         public void CallMethodWithoutParameters() {
             LibraryInvoke invoker = new LibraryInvoke();
 
-            invoker.GenericInvoke(_chest.dllFile, _chest.className, "InputVoidReturnArray");
+            invoker.GenericInvoke(_chest.dllFile, _chest.className, Chest.InputVoidReturnArray);
         }
 
         [Test]
         public void CalledMethodWithoutParametersReturnAnswer() {
             LibraryInvoke invoker = new LibraryInvoke();
 
-            object[] answer = invoker.GenericInvoke(_chest.dllFile, _chest.className, "InputVoidReturnArray");
+            object[] answer = invoker.GenericInvoke(_chest.dllFile, _chest.className, Chest.InputVoidReturnArray);
 
-            Assert.AreEqual("first", answer[0]);
-            Assert.AreEqual("second", answer[1]);
+            Assert.AreEqual(Chest.StringFirst, answer[0]);
+            Assert.AreEqual(Chest.StringSecond, answer[1]);
         }
 
         [Test]
         public void CalledMethodWithoutParametersReturnCorrectTypeAnswer() {
             LibraryInvoke invoker = new LibraryInvoke();
 
-            object[] answer = invoker.GenericInvoke(_chest.dllFile, _chest.className, "InputVoidReturnArray");
+            object[] answer = invoker.GenericInvoke(_chest.dllFile, _chest.className, Chest.InputVoidReturnArray);
 
             Assert.AreEqual("".GetType(), answer[0].GetType());
             Assert.AreEqual("".GetType(), answer[1].GetType());
@@ -48,10 +47,10 @@ namespace TestCSharpInteropService {
 
         #region WithParameters
         [Test]
-        [TestCase("InputIntReturnArray", 1, 2)]
-        [TestCase("InputStringReturnArray", "first", "second")]
-        [TestCase("InputDoubleReturnArray", 1.0, 2.0)]
-        [TestCase("InputObjectReturnArray", "first", 2.0)]
+        [TestCase(Chest.InputIntReturnArray, Chest.IntFirst, Chest.IntSecond)]
+        [TestCase(Chest.InputStringReturnArray, Chest.StringFirst, Chest.StringSecond)]
+        [TestCase(Chest.InputDoubleReturnArray, Chest.DoubleFirst, Chest.DoubleSecond)]
+        [TestCase(Chest.InputObjectReturnArray, Chest.StringFirst, Chest.DoubleSecond)]
         public void CallMethodWithParameters(string method, object first, object second) {
             LibraryInvoke invoker = new LibraryInvoke();
             object[] parameters = new object[] { first, second };
@@ -60,10 +59,10 @@ namespace TestCSharpInteropService {
         }
 
         [Test]
-        [TestCase("InputIntReturnArray", 1, 2)]
-        [TestCase("InputStringReturnArray", "first", "second")]
-        [TestCase("InputDoubleReturnArray", 1.0, 2.0)]
-        [TestCase("InputObjectReturnArray", "first", 2.0)]
+        [TestCase(Chest.InputIntReturnArray, Chest.IntFirst, Chest.IntSecond)]
+        [TestCase(Chest.InputStringReturnArray, Chest.StringFirst, Chest.StringSecond)]
+        [TestCase(Chest.InputDoubleReturnArray, Chest.DoubleFirst, Chest.DoubleSecond)]
+        [TestCase(Chest.InputObjectReturnArray, Chest.StringFirst, Chest.DoubleSecond)]
         public void CalledMethodWithParametersReturnAnswer(string method, object first, object second) {
             LibraryInvoke invoker = new LibraryInvoke();
             object[] parameters = new object[] { first, second };
@@ -75,10 +74,10 @@ namespace TestCSharpInteropService {
         }
 
         [Test]
-        [TestCase("InputIntReturnArray", 1, 2)]
-        [TestCase("InputStringReturnArray", "first", "second")]
-        [TestCase("InputDoubleReturnArray", 1.0, 2.0)]
-        [TestCase("InputObjectReturnArray", "first", 2.0)]
+        [TestCase(Chest.InputIntReturnArray, Chest.IntFirst, Chest.IntSecond)]
+        [TestCase(Chest.InputStringReturnArray, Chest.StringFirst, Chest.StringSecond)]
+        [TestCase(Chest.InputDoubleReturnArray, Chest.DoubleFirst, Chest.DoubleSecond)]
+        [TestCase(Chest.InputObjectReturnArray, Chest.StringFirst, Chest.DoubleSecond)]
         public void CalledMethodWithParametersReturnCorrectTypeAnswer(string method, object first, object second) {
             LibraryInvoke invoker = new LibraryInvoke();
             object[] parameters = new object[] { first, second };
